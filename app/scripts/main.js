447a4147e730cd145b861b54d64132dc79f6ac1d
Albert Lin <ksin751119@gmail.com>
2018-04-06 00:08:47 +0800

    fit code sytle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,14 @@ var nodeType;
 var abiDefinitionString = '[{"constant":false,"inputs":[{"name":"yourName","type":"bytes32"}],"name":"interact","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"currentName","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"fromAddres","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lastUpdatedMinutes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"name","type":"bytes32"},{"indexed":true,"name":"addr","type":"address"},{"indexed":true,"name":"timeUpdated","type":"uint256"}],"name":"Interaction","type":"event"}]'
 
 var contractEvent;
-var contractEventCounter=0;
+var contractEventCounter = 0;
 var defaultContractAddress = '0x10fcfbba14a146856f2644f05fb0fc134ffeb033';
+
+
 /**
  * Get the version information for Web3
  */
-
-
- window.addEventListener('load', function() {
-
+window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
@@ -24,97 +23,90 @@ var defaultContractAddress = '0x10fcfbba14a146856f2644f05fb0fc134ffeb033';
     var provider = document.getElementById('provider_url').value;
     window.web3 = new Web3(new Web3.providers.HttpProvider(provider));
   }
-
-  // Now you can start your app & access web3 freely:
   startApp()
-
 })
 
 
-function    setWeb3Version() {
+/*Set Web3 Version*/
+function setWeb3Version() {
+  var versionJson = {};
+  // Asynchronous version
+  web3.version.getNode(function(error, result) {
+    if (error) setData('version_information', error);
+    else {
+      setData('version_information', result);
 
-    var versionJson = {};
-
-    // Asynchronous version
-    web3.version.getNode(function(error, result){
-        if(error) setData('version_information',error);
-        else {
-            setData('version_information',result);
-
-            if(result.toLowerCase().includes('metamask')){
-                nodeType = 'metamask';
-            } else if(result.toLowerCase().includes('testrpc')){
-                nodeType = 'testrpc';
-            } else {
-                nodeType = 'geth';
-            }
-        }
-    });
-}
-
-
-function    doGetAccounts() {
-    // This is the synch call for getting the accounts
-    // var accounts = web3.eth.accounts
-
-    // Asynchronous call to get the accounts
-    // result = [Array of accounts]
-    // MetaMask returns 1 account in the array - that is the currently selected account
-    web3.eth.getAccounts(function (error, result) {
-        if (error) {
-            setData('accounts_count', error, true);
-        } else {
-            // You need to have at least 1 account to proceed
-            if(result.length == 0) {
-                if(nodeType == 'metamask'){
-                    alert('Unlock MetaMask *and* click \'Get Accounts\'');
-                }
-                return;
-            }
-
-            var defaultAccount = web3.eth.defaultAccount;
-            if(!defaultAccount){
-                web3.eth.defaultAccount =  result[0];
-                defaultAccount = '[Undef]' + result[0];
-            }
-            setDataValue('userAccount', defaultAccount, true);
-        }
-
-        // This populates the SELECT boxes with the accounts
-        //addAccountsToSelects(accounts);
-    });
-}
-
-function    startApp(){
-
-    // If the app is reconnected we should reset the watch
-    //doFilterStopWatching();
-    //doContractEventWatchStop();
-
-    // Set the connect status on the app
-    if (web3 && web3.isConnected()) {
-        setData('connect_status','Connected');
-
-        // Gets the version data and populates the result UI
-        setWeb3Version();
-        doGetAccounts();
-        doContractEventWatchStart();
-        doContractFunctionCall();
-
-    } else {
-        setData('connect_status','Not Connected');
+      if (result.toLowerCase().includes('metamask')) {
+        nodeType = 'metamask';
+      } else if (result.toLowerCase().includes('testrpc')) {
+        nodeType = 'testrpc';
+      } else {
+        nodeType = 'geth';
+      }
     }
-
-    return;
+  });
 }
 
-function doConnect()    {
 
-    // Get the provider URL
-    var provider = document.getElementById('provider_url').value;
-    //var provider = document.getElementById('provider_url').value;
-    window.web3 = new Web3(new Web3.providers.HttpProvider(provider));
-    startApp();
+function doGetAccounts() {
+  // This is the synch call for getting the accounts
+  // var accounts = web3.eth.accounts
+
+  // Asynchronous call to get the accounts
+  // result = [Array of accounts]
+  // MetaMask returns 1 account in the array - that is the currently selected account
+  web3.eth.getAccounts(function(error, result) {
+    if (error) {
+      setData('accounts_count', error, true);
+    } else {
+      // You need to have at least 1 account to proceed
+      if (result.length == 0) {
+        if (nodeType == 'metamask') {
+          alert('Please Unlock MetaMask and Reload Page');
+        }
+        return;
+      }
+
+      var defaultAccount = web3.eth.defaultAccount;
+      if (!defaultAccount) {
+        web3.eth.defaultAccount = result[0];
+        defaultAccount = '[Undef]' + result[0];
+      }
+      setDataValue('userAccount', defaultAccount, true);
+    }
+  });
+}
+
+function startApp() {
+
+  // If the app is reconnected we should reset the watch
+  //doFilterStopWatching();
+  //doContractEventWatchStop();
+
+  // Set the connect status on the app
+  if (web3 && web3.isConnected()) {
+    setData('connect_status', 'Connected');
+
+    // Gets the version data and populates the result UI
+    setWeb3Version();
+    doGetAccounts();
+    doContractEventWatchStart();
+    doContractFunctionCall();
+
+  } else {
+    setData('connect_status', 'Not Connected');
+  }
+
+  return;
+}
+
+function doConnect() {
+
+  // Get the provider URL
+  var provider = document.getElementById('provider_url').value;
+  //var provider = document.getElementById('provider_url').value;
+  window.web3 = new Web3(new Web3.providers.HttpProvider(provider));
+  startApp();
 
 }
 
@@ -122,145 +114,126 @@ function doConnect()    {
 /**
  * To stop the event watching using the contract object
  */
-
-function    doContractEventWatchStop()   {
-
-    if(contractEvent){
-        contractEvent.stopWatching();
-        contractEvent = undefined;
-    }
-    contractEventCounter = 0;
-    clearTable('eventTable');
+function doContractEventWatchStop() {
+  if (contractEvent) {
+    contractEvent.stopWatching();
+    contractEvent = undefined;
+  }
+  contractEventCounter = 0;
+  clearTable('eventTable');
 }
+
 
 /**
  * To start the event watching using the contract object
  */
+function doContractEventWatchStart() {
 
-function    doContractEventWatchStart() {
+  if (contractEvent) {
+    doContractEventWatchStop();
+  }
 
-    if(contractEvent){
-        doContractEventWatchStop();
+  contractEvent = createContractEventInstance();
+  contractEvent.watch(function(error, result) {
+    if (error) {
+      console.error('Contract Event Error');
+    } else {
+      //    console.log("Event.watch="+JSON.stringify(result))
+      // increment the count watch_instance_event_count
+      contractEventCounter++;
+      //setData('watch_contract_instance_event_count',contractEventCounter);
+
+      var watchEvent = {
+        'name': web3.toAscii(result['args']['name']),
+        'addr': result['args']['addr'],
+        'date': (new Date(1000 * result['args']['timeUpdated'])).toUTCString()
+      }
+
+      addEventTableItem('eventTable', watchEvent);
+      setData('txn_link', '');
     }
-
-    // Reset the UI
-    //setData('watch_contract_instance_event_count','0');
-    //console.log(contractEventCounter);
-    //watch_contract_instance_event_count = 0;
-    contractEvent = createContractEventInstance();
-    //console.log('XXXXX');
-
-
-    contractEvent.watch(function(error, result){
-        if(error){
-            console.error('Contract Event Error');
-        } else {
-            //    console.log("Event.watch="+JSON.stringify(result))
-            // increment the count watch_instance_event_count
-            contractEventCounter++;
-            //setData('watch_contract_instance_event_count',contractEventCounter);
-
-            var watchEvent = {
-              'name': web3.toAscii(result['args']['name']),
-              'addr': result['args']['addr'],
-              'date': (new Date(1000 * result['args']['timeUpdated'])).toUTCString()
-            }
-
-            addEventTableItem('eventTable', watchEvent);
-            setData('txn_link', '');
-        }
-    });
+  });
 }
 
 
 /**
  * Utility method for creating an instance of the event
  */
-function createContractEventInstance(){
-    var contractAddress = document.getElementById('contract_instance_address').value
-    console.log("contract Address: " + contractAddress);
+function createContractEventInstance() {
+  var contractAddress = document.getElementById('contract_instance_address').value
+  console.log("contract Address: " + contractAddress);
 
+  var contractInstance = createContractInstance(contractAddress);
+  var indexedEventValues = '{}';
+  indexedEventValues = JSON.parse(indexedEventValues)
 
-    var contractInstance = createContractInstance(contractAddress);
-
-    // geth the indexed data values JSON
-
-    //var indexedEventValues = document.getElementById('indexed_event_values').value
-    var indexedEventValues = '{}';
-    indexedEventValues = JSON.parse(indexedEventValues)
-
-    //var additionalFilterOptions = document.getElementById('additional_filter_event_values').value;
-    var additionalFilterOptions = {
-        "fromBlock": 0,
-        "toBlock": "latest"
-    }
-    //additionalFilterOptions = JSON.parse(additionalFilterOptions);
-
-    //console.log(indexedEventValues);
-    //console.log(additionalFilterOptions);
-    return contractInstance.Interaction(indexedEventValues, additionalFilterOptions);
+  var additionalFilterOptions = {
+    "fromBlock": 0,
+    "toBlock": "latest"
+  }
+  return contractInstance.Interaction(indexedEventValues, additionalFilterOptions);
 }
 
 
 // Utility method for creating the contract instance
-function  createContractInstance(addr){
-    //var     abiDefinitionString = document.getElementById('compiled_abidefinition').value;
-    var     abiDefinition = JSON.parse(abiDefinitionString);
+function createContractInstance(addr) {
+  var abiDefinition = JSON.parse(abiDefinitionString);
 
-    // Instance uses the definition to create the function
+  // Instance uses the definition to create the function
+  var contract = web3.eth.contract(abiDefinition);
+  var address = addr;
 
-    var    contract = web3.eth.contract(abiDefinition);
-    var    address = addr;
+  if (!address) address = document.getElementById('contract_instance_address').value;
+  var instance = contract.at(address);
 
-    if(!address) address = document.getElementById('contract_instance_address').value;
-    var    instance = contract.at(address);
-
-    return instance;
+  return instance;
 }
+
 
 /**
  * send Transaction costs Gas. State changes are recorded on the chain.
  */
-function    doContractSendCall()   {
-    // creating the cntract instance
-    var instance = createContractInstance();
-    // read the ui elements
-    var estimatedGas = 4500000;
-    var something = document.getElementById('something').value;
+function doContractSendCall() {
+  // creating the cntract instance
+  var instance = createContractInstance();
+  // read the ui elements
+  var estimatedGas = 4500000;
+  var something = document.getElementById('something').value;
 
-    // Create the transaction object
-    var    txnObject = {
-        from: web3.eth.defaultAccount,
-        gas: estimatedGas
+  // Create the transaction object
+  var txnObject = {
+    from: web3.eth.defaultAccount,
+    gas: estimatedGas
+  }
+
+  instance.interact.sendTransaction(something, txnObject, function(error, result) {
+    console.log('RECVED>>', error, result);
+    if (error) {
+      console.log(error);
+    } else {
+
+      console.log(result);
+      setEtherscanIoLink('txn_link', 'tx', result);
+
     }
-
-    instance.interact.sendTransaction(something,txnObject,function(error, result)  {
-        console.log('RECVED>>',error,result);
-        if(error){
-            console.log(error);
-        } else {
-
-            console.log(result);
-            setEtherscanIoLink('txn_link', 'tx', result);
-
-        }
-    });
+  });
 }
+
 
 /**
  * This invokes the contract function
  * locally on the node with no state change propagation
  */
-function    doContractFunctionCall()  {
-    // This leads to the invocation of the method locally
-    var instance = createContractInstance();
-    instance.currentName.call({},web3.eth.defaultBlock, function(error,result){
-      if(error) {
-        console.log(error);
-      } else {
-        console.log(result);
-      }
-    });
+function doContractFunctionCall() {
+  // This leads to the invocation of the method locally
+  var instance = createContractInstance();
+  instance.currentName.call({}, web3.eth.defaultBlock, function(error, result) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(result);
+    }
+  });
 }
 
 
@@ -269,6 +242,7 @@ function updateContractAddress() {
   document.getElementById('contract_instance_address').value = contractAddress;
   startApp();
 }
+
 
 function resetContractAddress() {
   document.getElementById('contract_instance_address').value = defaultContractAddress;
--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -1,98 +1,98 @@
 // Holds the base URL for etherscan.io
-var etherscanBaseUrl='https://ropsten.etherscan.io/';
+var etherscanBaseUrl = 'https://ropsten.etherscan.io/';
 
-
+// Set Data to innerHTML
 function setData(docElementId, html) {
-    document.getElementById(docElementId).innerHTML = html;
+  document.getElementById(docElementId).innerHTML = html;
 }
 
-function setDataValue(docElementId, value, disabled=false) {
-    document.getElementById(docElementId).value = value;
-    document.getElementById(docElementId).disabled = disabled;
+
+// Set Data to input value
+function setDataValue(docElementId, value, disabled = false) {
+  document.getElementById(docElementId).value = value;
+  document.getElementById(docElementId).disabled = disabled;
 }
 
 
 /**
  * Sets the href in the <a> tag for etherscan.io
  */
-function   setEtherscanIoLink(aId, type, hashOrNumber){
-    var etherscanLinkA=document.getElementById(aId);
-    etherscanLinkA.href = createEtherscanIoUrl(type,hashOrNumber);
-    if(hashOrNumber)
-        etherscanLinkA.innerHTML='etherscan.io';
-    else
-        etherscanLinkA.innerHTML='';
+function setEtherscanIoLink(aId, type, hashOrNumber) {
+  var etherscanLinkA = document.getElementById(aId);
+  etherscanLinkA.href = createEtherscanIoUrl(type, hashOrNumber);
+  if (hashOrNumber)
+    etherscanLinkA.innerHTML = 'etherscan.io';
+  else
+    etherscanLinkA.innerHTML = '';
 }
+
 
 /**
  * Create the etherscan link
  */
-function    createEtherscanIoUrl(type,hashOrNumber){
+function createEtherscanIoUrl(type, hashOrNumber) {
+  // For TestRPC - this URL will have no meaning as the
+  // Etherscan.io will not know about the Tx Hash
 
-    // For TestRPC - this URL will have no meaning as the
-    // Etherscan.io will not know about the Tx Hash
-
-    var url = etherscanBaseUrl;
-    if(type === 'tx'){
-        url += 'tx/'+hashOrNumber;
-    } else if(type === 'block'){
-        url += 'block/'+hashOrNumber;
-    } else if(type === 'address'){
-        url += 'address/'+hashOrNumber;
-    }
-    return url;
+  var url = etherscanBaseUrl;
+  if (type === 'tx') {
+    url += 'tx/' + hashOrNumber;
+  } else if (type === 'block') {
+    url += 'block/' + hashOrNumber;
+  } else if (type === 'address') {
+    url += 'address/' + hashOrNumber;
+  }
+  return url;
 }
+
 
 /**
  * Adds a list element in the 0th position
  * Removes the last element if the length exceeds provided ln
  */
-function    addEventTableItem(tableId, childData, len){
+function addEventTableItem(tableId, childData, len) {
+  var table = document.getElementById(tableId);
+  var num = table.rows.length
+  console.log("table length" + num)
+  //console.log(num);
+  if (table.rows.length >= len) {
+    var i = table.rows.length - 1; // last child
+    table.deleteRow(i);
+  }
 
-    //console.log('Event:',childData);
+  var Tr = table.insertRow(0);
+  console.log(Tr.cells.length);
+  var Td = Tr.insertCell(Tr.cells.length);
+  var p = document.createElement('A');
+  p.text = childData.name;
+  Td.appendChild(p);
 
-    // check length
-    var table = document.getElementById(tableId);
-    var num = table.rows.length
-    console.log("table length" + num)
-    //console.log(num);
-    if(table.rows.length >= len){
-        var i = table.rows.length - 1; // last child
-        table.deleteRow(i);
-    }
+  Td = Tr.insertCell(Tr.cells.length);
+  Td.width = '200%'
+  Td.align = "middle"
+  var p = document.createElement('A');
+  p.text = childData.date;
+  Td.appendChild(p);
 
-    var Tr = table.insertRow(0);
-    console.log(Tr.cells.length);
-    var Td = Tr.insertCell(Tr.cells.length);
-    var p = document.createElement('A');
-    p.text = childData.name;
-    Td.appendChild(p);
-
-    Td = Tr.insertCell(Tr.cells.length);
-    Td.width = '200%'
-    Td.align="middle"
-    var p = document.createElement('A');
-    p.text = childData.date;
-    Td.appendChild(p);
-
-    Td = Tr.insertCell(Tr.cells.length);
-    var aLink = document.createElement('A')
-    aLink.text = childData.addr;
-    aLink.href = createEtherscanIoUrl('address', childData.addr);
-    aLink.target='_blank';
-    Td.width = "25%"
-    Td.align = "right"
-    Td.appendChild(aLink);
+  Td = Tr.insertCell(Tr.cells.length);
+  var aLink = document.createElement('A')
+  aLink.text = childData.addr;
+  aLink.href = createEtherscanIoUrl('address', childData.addr);
+  aLink.target = '_blank';
+  Td.width = "25%"
+  Td.align = "right"
+  Td.appendChild(aLink);
 }
+
 
 /**
  * Clears the items from a list element on the HTML page
  */
-function    clearTable(tableId){
-    var table = document.getElementById(tableId);
-    console.log("length" + table.childNodes.length);
-    for(var i=table.childNodes.length-1; i>=0 ;i--){
-        //console.log(table.childNodes[i]);
-        table.removeChild(table.childNodes[i]);
-    }
+function clearTable(tableId) {
+  var table = document.getElementById(tableId);
+  console.log("length" + table.childNodes.length);
+  for (var i = table.childNodes.length - 1; i >= 0; i--) {
+    //console.log(table.childNodes[i]);
+    table.removeChild(table.childNodes[i]);
+  }
 }
